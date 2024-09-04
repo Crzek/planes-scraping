a631fc967c5271c449c32ad9ba71d41472e71073
cruz-tpx1-kali <javier1996.ec@gmail.com>
2024-09-04 21:59:08 +0200

    ultimas actualizaciones para que funcione en 2n plano

--- app/utils/wdriver.py
+++ app/utils/wdriver.py
@@ -22,6 +22,7 @@ url = os.getenv("base_url")
 # Configura las opciones de Brave
 options = webdriver.ChromeOptions()
 # no sandbox
+# configuración de la memoria para docker
 options.add_argument('--no-sandbox')
 options.add_argument('--headless')
 options.add_argument('--disable-dev-shm-usage')
