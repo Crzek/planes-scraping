8fcf85d87e4c4f748cc3dd65d79a8d1d77a90c9a
erick-tpx1 <javier1996.ec@gmail.com>
2025-04-23 03:27:25 +0200

    logger in this files

--- extencions.py
+++ extencions.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,19 +7,21 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 login_manager = LoginManager()
 
+logger = logging.getLogger(__name__)
+
 
 @login_manager.user_loader
 def load_user(user_id):  # user_id es un string 3
     from src.auth.models.users import User  # nopep8
     # metodos staticos de la clase User
-    print("load_user", user_id, type(user_id))
+    logger.info("load_user: %s - id:%s", user_id, type(user_id))
     user = User.get_user_db(user_id=user_id)
     return user
 
 
 def get_file() -> str:
     name = os.getenv("CONFIG_ENV")
-    print(f"***getenv** {name}--len:", len(name), type(name))
+    logger.info(f"***getenv** {name}--len:", len(name), type(name))
     if (name == "development"):
         f = "config/dev.py"
     elif (name == "production-amd"):  # (name == "production")
--- globals.py
+++ globals.py
@@ -1,15 +1,18 @@
 from dotenv import dotenv_values
 import os
 import datetime
+import logging
+
+logger = logging.getLogger(__name__)
 
 ENV_FILE = os.getenv("ENV_FILE",)
 # Asegúrate de que no se cargue el archivo .env por defecto
 env_values = dotenv_values(ENV_FILE, verbose=True)
 
 if env_values:
-    print("--- ENV_FILE: ", ENV_FILE, type(ENV_FILE))
-    print("--- Cargando: ", ENV_FILE)
-    print(".env Cargadas")
+    logger.info("--- ENV_FILE: %s, type: %s", ENV_FILE, type(ENV_FILE))
+    logger.info("--- Cargando: %s", ENV_FILE)
+    logger.info(".env Cargadas")
 
 PATH_STATIC = "static/"
 PATH_STATIC_DATA = PATH_STATIC + "data/"
@@ -27,7 +30,7 @@ URL = env_values.get("base_url")
 
 PATH_BROWSER = env_values.get("PATH_BROWSER", None)
 PATH_DRIVER = env_values.get("PATH_CHROMEDRIVER", None)
-print("--- PATH_BROWSER", PATH_BROWSER)
-print("--- PATH_DRIVER", PATH_DRIVER)
+logger.info("--- PATH_BROWSER: %s", PATH_BROWSER)
+logger.info("--- PATH_DRIVER: %s", PATH_DRIVER)
 
 SQLALCHEMY_DATABASE_URL = env_values.get("SQLALCHEMY_DATABASE_URL")
